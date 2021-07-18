4d127fe395819a9fe00663abee931717e64cae52
Éder Cantanhede <edercantanhede@gmail.com>
2021-07-17 21:03:46 -0300

    Exercícios de hoje completos

---
+++ funções/pets1.1.py
@@ -0,0 +1,7 @@
+def descreve_pet(pet_name, animal_type='dog'):
+    """Exibe informações sobre um animal de estimação"""
+    print("\ntenho um " + animal_type + ".")
+    print("\nMeu " + animal_type + " se chama " + pet_name.title() + ".")
+
+
+descreve_pet("willie")
